67733bacb89d4398965f0fb73f4eca52e034564f
ros_swcole <sawyercole50@gmail.com>
2017-05-16 14:15:58 -0700

    alex

--- src/FinalMain.java
+++ src/FinalMain.java
@@ -4,23 +4,56 @@
 import javafx.event.Event;
 import javafx.scene.input.KeyEvent;
 
+import java.awt.event.KeyListener;
 import java.util.*;
 import java.applet.*;
-public class FinalMain {
+public class FinalMain extends Applet implements KeyListener{
+    Player player = new Player();
+    boolean playGame = true;
+    Scanner console = new Scanner(System.in);
+    Input input = new Input();
+    GameBoard board = new GameBoard();
+    AllLocations allLocations = new AllLocations();
+
     public static void main(String[] args) {
-        Player player = new Player();
-        boolean playGame = true;
-        Scanner console = new Scanner(System.in);
-        Input input = new Input();
-        GameBoard board = new GameBoard();
-        AllLocations allLocations = new AllLocations();
 
         while (playGame) {
             Location currentLocation = allLocations.getLocation(player);
             board.drawBoard(player, allLocations.getLocation(player));
-            input.getUserInput(console, player, board);
             board.clearBoard(player, currentLocation);
         }
+
+    }
+    public FinalMain(){
+
     }
 
+    @Override
+    public void keyTyped(java.awt.event.KeyEvent e) {
+
+
+        if(e.getKeyCode() == java.awt.event.KeyEvent.VK_UP) {
+            player.up(1);
+        }
+        else if(e.getKeyCode() == java.awt.event.KeyEvent.VK_DOWN) {
+            player.down(1);
+        }
+        else if(e.getKeyCode() == java.awt.event.KeyEvent.VK_RIGHT) {
+            player.right(1);
+        }
+        else if(e.getKeyCode() == java.awt.event.KeyEvent.VK_LEFT) {
+            player.right(1);
+        }
+
+    }
+
+    @Override
+    public void keyPressed(java.awt.event.KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(java.awt.event.KeyEvent e) {
+
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -6,7 +6,7 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Player extends Applet implements KeyListener {
+public class Player {
     int hunger;
     private int x;
     private int y;
@@ -122,29 +122,4 @@ public class Player extends Applet implements KeyListener {
         }
     }
 
-    @Override
-    public void keyTyped(KeyEvent e) {
-        if(e.getKeyCode() == KeyEvent.VK_UP) {
-            up(1);
-        }
-        else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
-            down(1);
-        }
-        else if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            right(1);
-        }
-        else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
-            right(1);
-        }
-    }
-
-    @Override
-    public void keyPressed(KeyEvent keyEvent) {
-
-    }
-
-    @Override
-    public void keyReleased(KeyEvent keyEvent) {
-
-    }
 }
